4014e8d5e0ed159730199729e2ca3613043090bc
Nazar Kuzmenko <nazar@streamcrease.com>
2017-03-27 02:49:42 +0300

    feat(logger): creates logger module

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const config = require('config')
 const helmet = require('helmet')
+const morgan = require('morgan')
+
+const log = require('./modules/logger')
 
 const app = express()
 
 app.use(helmet())
+app.use(morgan('tiny', { stream: log.stream }))
 
 app.listen(config.get('server.port'), () => {
-  console.info(`Server is running on ${config.get('server.port')} port`)
+  log.info(`Server is running on ${config.get('server.port')} port`)
 })
