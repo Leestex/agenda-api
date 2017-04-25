915c82a9f24b1dffec8247512a129b284adc6823
Nazar Kuzmenko <nazar.kuzmenko@gmail.com>
2017-04-25 12:20:55 +0300

    feat(app): adds body parser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import bodyParser from 'body-parser'
 
 import router from './router'
 import log from './modules/logger'
@@ -9,6 +10,8 @@ const app = express()
 
 app.use(helmet())
 app.use(morgan('tiny', { stream: log.stream }))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(router)
 
 export default app
